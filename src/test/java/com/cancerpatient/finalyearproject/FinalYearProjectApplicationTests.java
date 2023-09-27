8420ba456a5183bb354f73f1b44a4081d66f833b
kasthuriarachchi996 <www.pasindudenuka5770@gmail.com>
2023-09-27 19:33:34 +0530

    create project

---
+++ src/test/java/com/cancerpatient/finalyearproject/FinalYearProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cancerpatient.finalyearproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalYearProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
